b9c6e5a22bf54564ba083ab852ca48190eceff96
Miguel Santamaria <santamaria.migue@gmail.com>
2016-10-30 23:04:26 -0500

    TB

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
 var users = require('./routes/users');
+var tb = require('./routes/tb');
 
 var app = express();
 
@@ -24,8 +24,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
 app.use('/users', users);
+app.use('/tb', tb);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
